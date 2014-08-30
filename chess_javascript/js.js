5827689b7b0a78350964a2acce53dedf97535080
sushantsakurde11 <sushantsakurde11@gmail.com>
2014-08-30 20:06:44 +0530

    drag and drop added

---
+++ chess_javascript/js.js
@@ -0,0 +1,17 @@
+function myFunction()
+{
+document.getElementById("ss").src="images/king_black.png";
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
