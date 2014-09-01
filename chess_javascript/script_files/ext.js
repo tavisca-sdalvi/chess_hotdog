0313b51eeccd6ccad5e90ddca2c382ed60ea1bae
swapnildalvi90 <sdalvi@tavisca.com>
2014-09-01 15:11:03 +0530

    all alerts commited

--- chess_javascript/script_files/ext.js
+++ chess_javascript/script_files/ext.js
@@ -16,7 +16,7 @@ pawn_white="<img src='images/pawn_white.png' name='pw'>";
 
 function abc()
 {
-alert("hello");
+//alert("hello");
 document.getElementById("11").innerHTML=rook_black;
 document.getElementById("11").setAttribute("onclick","myglobal(this)");
 document.getElementById("11").style.cursor = "pointer";
@@ -140,24 +140,24 @@ alert("in main loop");
 
 function myglobal(obj)
 {
-alert("global");
+//alert("global");
 divid=obj.id;
-alert(divid);
+//alert(divid);
 var divx=parseInt(divid.charAt(0));
 var divy=parseInt(divid.charAt(1));
 
 var tds=document.getElementById(divid).firstChild.name;
-alert(tds);
-//alert("in global");
+//alert(tds);
+////alert("in global");
 myMove(tds,divx,divy);
-alert("okay");
+//alert("okay");
 }
 
 function myput(obj)
 {
-	//alert("hello");
+	////alert("hello");
 	var target_divid=obj.id;
-	//alert(target_divid);
+	////alert(target_divid);
 	var divx=parseInt(target_divid.charAt(0));
 	var divy=parseInt(target_divid.charAt(1));
 	
@@ -187,45 +187,45 @@ function myput(obj)
 
 function myMove(tds,divx,divy)
 {
-	alert("in myMove");
+	//alert("in myMove");
 	if(tds=="rw"|tds=="rb")
 	{
-		alert("in if");
+		//alert("in if");
 		rmoves(divx,divy,tds);
 	}
 		//for bishop
 if(tds=="bw"|tds=="bb")
 {
-//alert("in if");
+////alert("in if");
 bmoves(divx,divy,tds);
 }
 //for queen
 	if(tds=="qw"|tds=="qb")
 	{
-		//alert("in if");
+		////alert("in if");
 		rmoves(divx,divy,tds);
 		bmoves(divx,divy,tds);
 	}
 	//for knight
 	if(tds=="hw"|tds=="hb")
 	{
-		alert("in hmoves");
+		//alert("in hmoves");
 		hmoves(divx,divy,tds);
 	}
 	//for pawn
 	if(tds=="pb")
 	{
-		alert("in pmoves");
+		//alert("in pmoves");
 		pbmoves(divx,divy,tds);
 	}
 	if(tds=="pw")
 	{
-		alert("in pmoves");
+		//alert("in pmoves");
 		pwmoves(divx,divy,tds);
 	}
 	if(tds=="kw" | tds=="kb")
 	{
-		alert("in kmoves");
+		//alert("in kmoves");
 		kmoves(divx,divy,tds);
 	}
 }
@@ -307,11 +307,11 @@ if(divx=="7")
 
 function rmoves(divx, divy,tds)
 {
-	//alert("in rmoves");
+	////alert("in rmoves");
 	//for horizontal right
 	for(i=(divy+1);i<=8;i++)
 	{
-		//alert("in for");
+		////alert("in for");
 		if(isEmpty(divx,i))
 		{
 			highlight(divx,i);
@@ -333,7 +333,7 @@ function rmoves(divx, divy,tds)
 	//for horizontal left
 	for(i=(divy-1);i>0;i--)
 	{
-		//alert("in reverse for");
+		////alert("in reverse for");
 		if(isEmpty(divx,i))
 		{
 
@@ -355,7 +355,7 @@ function rmoves(divx, divy,tds)
 
 	for(i=(divx+1);i<=8;i++)
 	{
-		//alert("in for");
+		////alert("in for");
 		if(isEmpty(i,divy))
 		{
 			highlight(i,divy);
@@ -376,7 +376,7 @@ function rmoves(divx, divy,tds)
 
 	for(i=(divx-1);i>0;i--)
 	{
-		//alert("in for");
+		////alert("in for");
 		if(isEmpty(i,divy))
 		{
 			highlight(i,divy);
@@ -401,11 +401,11 @@ function rmoves(divx, divy,tds)
 //for bishop 
 function bmoves(divx, divy,tds)
 {
-//alert("in rmoves");
+////alert("in rmoves");
 for(i=(divx+1),j=(divy+1);i<=8 && j<=8;i++,j++)
 {
-alert(i);
-alert(j);
+//alert(i);
+//alert(j);
 if(isEmpty(i,j))
 {
 highlight(i,j);
@@ -425,9 +425,9 @@ break;
 }
 for(i=(divx-1),j=(divy-1);i>0 && j>0;i--,j--)
 {
-//alert("in for");
-alert(i);
-alert(j);
+////alert("in for");
+//alert(i);
+//alert(j);
 
 if(isEmpty(i,j))
 {
@@ -447,13 +447,13 @@ break;
 }
 }
 //for vertical
-alert("in 3rd for");
+//alert("in 3rd for");
 for(k=(divx+1),l=(divy-1);k<=8 && l>0;k++,l--)
 	{
-		alert(k);
-alert(l);
+		//alert(k);
+//alert(l);
 
-		//alert("in for");
+		////alert("in for");
 		if(isEmpty(k,l))
 		{
 			highlight(k,l);
@@ -471,12 +471,12 @@ alert(l);
 			}
 		}
 	}
-alert("in 4th for");
+//alert("in 4th for");
 	for(i=(divx-1),j=(divy+1);i>0 && j<=8;i--,j++)
 	{
-		//alert("in for");
-		alert(i);
-alert(j);
+		////alert("in for");
+		//alert(i);
+//alert(j);
 
 		if(isEmpty(i,j))
 		{
@@ -499,7 +499,7 @@ alert(j);
 	//for knight
 function hmoves(divx, divy,tds)
 {
-	alert("in hmove");
+	//alert("in hmove");
 	if(((divx-2)>0)&&((divx-2)<=8)&&((divy+1)>0)&&((divy+1)<=8))
 	{
 		if(isEmpty(divx-2,divy+1))
@@ -599,7 +599,7 @@ highlight(divx-2,divy-1);
 
 function kmoves(divx,divy,tds)
 {
-	alert("in kmoves");
+	//alert("in kmoves");
 	for(i=divx-1;i<=divx+1;i++)
 	{
 		for(j=divy-1;j<=divy+1;j++)
@@ -610,20 +610,20 @@ function kmoves(divx,divy,tds)
 			{
 				if(i==0 | j==0 |i==9|j==9)
 				{
-					alert("i=0 or j=0");
+					//alert("i=0 or j=0");
 					continue;
 				}
 				else
 				{
 					if(isEmpty(i,j))
 					{
-						alert("i,j is empty");
+						//alert("i,j is empty");
 						highlight(i,j);
 					}
 					else
 						if(!isFriend(i,j,tds))
 						{
-							alert("i,j is enemy");
+							//alert("i,j is enemy");
 						highlight(i,j);
 						
 						}
@@ -638,18 +638,18 @@ function kmoves(divx,divy,tds)
 
 function isEmpty (divx,i)
 {
-	//alert("in isempty");
+	////alert("in isempty");
 	var divstr=""+divx+i;
 	var imgvar=document.getElementById(divstr).getElementsByTagName("img")[0];
-	//alert(imgvar);
+	////alert(imgvar);
 	if(imgvar)
 	{
-		//alert("imgvar has some value");
+		////alert("imgvar has some value");
 		return false;
 	}
 	else
 	{
-		//alert("imgvar has no value");
+		////alert("imgvar has no value");
 			return true;
 	}
 		
@@ -677,7 +677,7 @@ function isFriend(divx,i,tds)
 
 function highlight(divx,i)
 {
-	//alert("in high");
+	////alert("in high");
 	var divstr=""+divx+i;
 	document.getElementById(divstr).setAttribute("style","border:2px solid red;");
 	document.getElementById(divstr).setAttribute("onclick","myput(this)");
